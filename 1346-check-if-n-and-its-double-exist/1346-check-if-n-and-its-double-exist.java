919a59c8e3955d7685a23689ec5185ed80c48e82
Shubham Teredesai <79035517+iamclickify@users.noreply.github.com>
2026-01-15 21:12:09 +0530

    Time: 2 ms (96.28%), Space: 44.6 MB (72.26%) - LeetHub

--- 1346-check-if-n-and-its-double-exist/1346-check-if-n-and-its-double-exist.java
+++ 1346-check-if-n-and-its-double-exist/1346-check-if-n-and-its-double-exist.java
@@ -2,8 +2,7 @@ class Solution {
     public boolean checkIfExist(int[] arr) {
         for (int i =0;i<arr.length;i++){
             for (int j=0;j<arr.length;j++){
-                if (arr[i] == 0) return false;
-                if (arr[i] == 2*arr[j]){
+                if (i != j && arr[i] == 2*arr[j]){
                     return true;
                 }
             }
