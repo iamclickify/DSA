ff676b5c35078ee7e5d2a303304898c3295c14d1
Shubham Teredesai <79035517+iamclickify@users.noreply.github.com>
2026-07-25 13:02:52 +0530

    Time: 0 ms (100%), Space: 29.4 MB (94.75%) - LeetHub

--- 0189-rotate-array/0189-rotate-array.cpp
+++ 0189-rotate-array/0189-rotate-array.cpp
@@ -8,9 +8,7 @@ public:
         reverse(nums, 0, k-1);
         reverse(nums, k, n-1);
     }
-
     void reverse(vector<int>& nums, int i, int j){
-        
         while(i<j){
             int temp = nums[i];
             nums[i] = nums[j];
@@ -19,6 +17,5 @@ public:
             i++;
             j--;
         }
-        
     }
 };
